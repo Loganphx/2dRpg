06aab562cc4a478e0079ba3cb5c6cae163e4c30b
Logan Ingram <65008630+Loganphx@users.noreply.github.com>
2020-11-28 18:29:32 -0800

    Added Health System.

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+public class Heart : MonoBehaviour
+{
+    public float amount;
+    public Sprite icon;
+
+    /// <summary>
+    /// Instantiates a new Heart.
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <param name="icon"></param>
+    public Heart(float amount, Sprite icon)
+    {
+        this.amount = amount;
+        this.icon = icon;
+    }
+    
+    /// <summary>
+    /// Duplicates a heart.
+    /// </summary>
+    /// <param name="heart"></param>
+    /// 
+    public Heart(Heart heart)
+    {
+        this.amount = heart.amount;
+        this.icon = heart.icon;
+    }
+}
---
+++ Assets/Scripts/UIHeart.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class UIHeart : MonoBehaviour
+{
+    public Heart heart;
+    private Image _spriteImage;
+    private UIHeart _selectedHeart;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _spriteImage = GetComponent<Image>();
+        UpdateHeart(heart);
+        _selectedHeart = GameObject.Find("SelectedItem").GetComponent<UIHeart>();
+
+    }
+
+    /// <summary>
+    /// Updates item's sprite and color
+    /// </summary>
+    /// <param name="item"></param>
+    public void UpdateHeart(Heart heart)
+    {
+        this.heart = heart;
+        if (this.heart != null)
+        {
+            _spriteImage.color = new Color(255, 255, 255, 255);
+            _spriteImage.sprite = this.heart.icon;
+            return;
+        }
+
+        _spriteImage.color = Color.clear;
+    }
+}
